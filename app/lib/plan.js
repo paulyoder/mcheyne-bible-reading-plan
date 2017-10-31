b34f01b22278ba932f3cbca1422d7ff08d307488
Paul Yoder <paulyoder@gmail.com>
2017-10-30 21:46:41 -0500

    Got it working

---
+++ app/lib/plan.js
@@ -0,0 +1,15 @@
+export const plan = {
+  1: {
+    1: ['Genesis 1', 'Matthew 1', 'Ezra 1', 'Acts 1'],
+    2: ['Genesis 2', 'Matthew 2', 'Ezra 2', 'Acts 2'],
+  },
+  10: {
+    30: ['2 Kings 11-12', '2 Timothy 2', 'Hosea 3-4', 'Psalm 119:121-144'],
+    31: ['2 Kings 13', '2 Timothy 3', 'Hosea 5-6', 'Psalm 119:145-176'],
+  },
+  11: {
+    1: ['2 Kings 14', '2 Timothy 4', 'Hosea 7', 'Psalm 120-122'],
+    2: ['2 Kings 15', 'Titus 1', 'Hosea 8', 'Psalm 123-125'],
+    3: ['2 Kings 16', 'Titus 2', 'Hosea 9', 'Psalm 126-128'],
+  }
+}
